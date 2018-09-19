cbbba217738d4059e8f0c420cad9547e5c8c59da
sergiolondono <sergio.londonog@gmail.com>
2018-09-19 16:59:47 -0500

    20180919.

--- startup/routes.js
+++ startup/routes.js
@@ -2,9 +2,11 @@ const express = require('express');
 const error = require('../middleware/error');
 
 const planPackages = require('../routes/planPackages');
+const customers = require('../routes/customers');
 
 module.exports = function(app){
     app.use(express.json());
     app.use('/api/planpackages', planPackages);
+    app.use('/api/customers', customers);
     app.use(error);
 }
