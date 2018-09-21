7189f45ad077768671255e143689c63530c3e981
sergiolondono <sergio.londonog@gmail.com>
2018-09-21 15:03:13 -0500

    20180921.

--- startup/routes.js
+++ startup/routes.js
@@ -4,6 +4,7 @@ const error = require('../middleware/error');
 const planPackages = require('../routes/planPackages');
 const customers = require('../routes/customers');
 const users = require('../routes/users');
+const simCards = require('../routes/simCards');
 const auth = require('../routes/auth');
 
 module.exports = function(app){
@@ -11,6 +12,7 @@ module.exports = function(app){
     app.use('/api/planpackages', planPackages);
     app.use('/api/customers', customers);
     app.use('/api/users', users);
+    app.use('/api/simCards', simCards);
     app.use('/api/auth', auth);
     app.use(error);
 }
