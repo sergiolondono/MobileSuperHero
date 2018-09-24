e55c8fddb654a303e382fcb4c14949483c69b2af
sergiolondono <sergio.londonog@gmail.com>
2018-09-24 11:42:53 -0500

    20180904..

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express();
 require('./startup/routes')(app);
 require('./startup/db')();
 require('./startup/validation')();
+require('./startup/prod')(app);
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => winston.info(`Listening on port ${port}...`));
